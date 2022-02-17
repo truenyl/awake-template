b745817ff6f6962c32f9c85b22a28479faaeb7d6
yuanlei <truenyl@163.com>
2022-02-17 12:07:26 +0800

    change tagline back

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -10,7 +10,7 @@ import theCMS from '../cms/netlify/cms'
  */
 export default {
   siteName: 'Awake',
-  tagline: 'test',
+  tagline: 'test tagline',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
